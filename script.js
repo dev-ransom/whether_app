47813ffa4d5339ba373a37bef04216f31025c161
idungstanley <sundaystanley56@gmail.com>
2023-10-09 10:47:40 +0100

    added code updating city name and round up the temperature value to a round figure

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+// select items using dom
+const temperature = document.querySelector('.temp'),cityName = document.querySelector('.city-name'), humidity = document.querySelector('.humidity'), windSpeed = document.querySelector('.wind-speed');
+
 const apiKey = '4b9b52090d23164fd44e153355922a7c';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?=&units=metric&q=kano';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?=&units=metric&q=jos';
 
 const searchBtn = document.querySelector('.searchBtn');
 searchBtn.addEventListener('click', getWeather);
@@ -7,5 +10,10 @@ searchBtn.addEventListener('click', getWeather);
 async function getWeather(){
     const response = await fetch(apiUrl + `&appid=${apiKey}`);
     let data = await response.json();
-    
+    // update city name
+    cityName.innerHTML = data.name;
+    // round up number
+    const round = Math.round(data.main.temp);
+    temperature.innerHTML = round;
+    console.log(data)
 }
