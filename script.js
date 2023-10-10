758f288dcd3a9afaec468194e730583152a05a45
idungstanley <sundaystanley56@gmail.com>
2023-10-10 08:15:23 +0100

    added code for finals

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // select items using dom
-const temperature = document.querySelector('.temp'),cityName = document.querySelector('.city-name'), humidity = document.querySelector('.humidity'), windSpeed = document.querySelector('.wind-speed'), inputField = document.querySelector('.search input'),   Weather = document.querySelector('.weather'), weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temp'),cityName = document.querySelector('.city-name'), humidity = document.querySelector('.humidity'), windSpeed = document.querySelector('.wind-speed'), inputField = document.querySelector('.search input'),   Weather = document.querySelector('.weather'), weatherIcon = document.querySelector('.weather-icon'), errmsg = document.querySelector('.error');
 
 const apiKey = '4b9b52090d23164fd44e153355922a7c';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?=&units=metric&q=';
@@ -11,16 +11,18 @@ async function getWeather(){
     const city = inputField.value.trim();
     if(city == ''){
         Weather.style.display = 'none'
-        // alert('Please enter a city name.');
+        alert('Please enter a city name.');
     return;
     }
 
   
     const response = await fetch(apiUrl + city +  `&appid=${apiKey}`);
-    // if(!response.ok){
-    //     alert('City not found.')
-    // }
-    let data = await response.json();
+    if(!response.ok){
+        errmsg.style.display = 'block';
+        Weather.style.display = 'none'
+        return;
+    }else{
+        let data = await response.json();
     // update city name
     cityName.innerHTML = data.name;
     // round up number and update temperature
@@ -38,18 +40,17 @@ async function getWeather(){
     }else if(data.weather[0].main == 'Clear'){
         weatherIcon.src = 'images/clear.png'
    }
-    
     else if(data.weather[0].main == 'Drizzle'){
         weatherIcon.src = 'images/drizzle.png'
    }
-    
     else if(data.weather[0].main == 'Mist'){
         weatherIcon.src = 'images/mist.png'
    }
-    
    else if(data.weather[0].main == 'Rain'){
         weatherIcon.src = 'images/rain.png'
    }
-
+   errmsg.style.display = 'none'
    Weather.style.display = 'block'
+    }
+    
 }
